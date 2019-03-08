b7014c02ed651513790e946e5ada68abb340f0a9
Onkar Singh <onkarsinghmnit@gmail.com>
2019-03-08 14:04:49 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  { path: 'register', loadChildren: './auth/register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from  '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { tap } from  'rxjs/operators';
+import { Observable, BehaviorSubject } from  'rxjs';
+
+import { Storage } from  '@ionic/storage';
+import { User } from  './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  AUTH_SERVER_ADDRESS:  string  =  '/api';
+  authSubject  =  new  BehaviorSubject(false);
+
+  constructor(private  httpClient:  HttpClient, private  storage:  Storage) { }
+
+  register(user: User): Observable<any> {
+    return this.httpClient.post<any>(`${this.AUTH_SERVER_ADDRESS}/register`, user).pipe(
+      tap(async (res:  any ) => {
+
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.access_token);
+          await this.storage.set("EXPIRES_IN", res.user.expires_in);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+
+  login(user: User): Observable<any> {
+    console.log(user);
+    return this.httpClient.post(`${this.AUTH_SERVER_ADDRESS}/login`, user).pipe(
+      tap(async (res: any) => {
+        console.log(res);
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.access_token);
+          await this.storage.set("EXPIRES_IN", res.user.expires_in);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+
+  async logout() {
+    await this.storage.remove("ACCESS_TOKEN");
+    await this.storage.remove("EXPIRES_IN");
+    this.authSubject.next(false);
+  }
+
+  isLoggedIn() {
+    return this.authSubject.asObservable();
+  }
+
+}
